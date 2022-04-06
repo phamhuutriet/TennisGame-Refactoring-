57f102d6f3a837613e0a5d42441e4e615600085e
Little Panda <>
2022-04-05 19:08:51 -0600

    Remove methods related to winning state in Tennis class

--- tennis.py
+++ tennis.py
@@ -100,21 +100,10 @@ class TennisGame2:
         return "Advantage " + self.player1.name()
       elif self.player2_is_advantage():
         return "Advantage " + self.player2.name()
-
-    def player1_is_winner(self):
-      return self.player1.point() >= 4 and self.player2.point() >= 0 and (self.player1.point()-self.player2.point()) >=2
-
-    def player2_is_winner(self):
-      return self.player2.point() >= 4 and self.player1.point() >= 0 and (self.player2.point()-self.player1.point()) >=2
-
+        
     def is_win_score(self):
-      return self.player1_is_winner() or self.player2_is_winner()
-
-    def handle_winner(self):
-      if self.player1_is_winner():
-        return "Win for " + self.player1.name()
-      if self.player2_is_winner():
-        return "Win for " + self.player2.name()
+      score_diff = abs(self.player1.point() - self.player2.point())
+      return score_diff >= 2 and (self.player1.point() >= 4 or self.player2.point() >= 4)
 
     def gamestate_factory(self):
       if self.is_win_score():
